0a77065a639e1cdd9f71f124e5afa56376262772
SJY <jihoseo2008@gmail.com>
2025-03-27 11:58:12 +0900

    edit cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,27 @@ import express from "express"
 import authRoutes from "./routes/authRoutes"
 import portfolioRoutes from "./routes/portfolioRoutes"
 import projectRoutes from "./routes/projectRoutes"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
+
+// 모든 OPTIONS 요청에 대해 CORS 헤더를 반환하도록 설정
+app.options(
+    "*",
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
 
 // 각 도메인 별 라우터 등록
 app.use("/auth", authRoutes)
